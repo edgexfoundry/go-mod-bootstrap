698b846c5bad4488a3f0aed0e9809f503303950e
Jennifer Williams <jennifer.m.williams@intel.com>
2022-03-28 14:29:31 -0700

    fix(config): ignore first change notification in ListenForCustomConfigChanges
    The first change notification causes issues with values that have
    been overriden. It needs to be ignored, similar to regular config
    ListenForChanges.
    
    Fixes #314
    
    Signed-off-by: Jennifer Williams <jennifer.m.williams@intel.com>

--- bootstrap/config/config.go
+++ bootstrap/config/config.go
@@ -307,6 +307,8 @@ func (cp *Processor) ListenForCustomConfigChanges(
 
 		configClient.WatchForChanges(updateStream, errorStream, configToWatch, sectionName)
 
+		isFirstUpdate := true
+
 		for {
 			select {
 			case <-cp.ctx.Done():
@@ -318,6 +320,14 @@ func (cp *Processor) ListenForCustomConfigChanges(
 				cp.lc.Error(ex.Error())
 
 			case raw := <-updateStream:
+				// Config Provider sends an update as soon as the watcher is connected even though there are not
+				// any changes to the configuration. This causes an issue during start-up if there is an
+				// envVars override of one of the Writable fields, so we must ignore the first update.
+				if isFirstUpdate {
+					isFirstUpdate = false
+					continue
+				}
+
 				cp.lc.Infof("Updated custom configuration '%s' has been received from the Configuration Provider", sectionName)
 				changedCallback(raw)
 			}
